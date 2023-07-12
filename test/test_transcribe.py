3132c3e925ff1c503eb2ef2196e6ccef1ccda564
Kazuki Kimura <kimura.kazuki44@gmail.com>
2023-07-13 06:33:47 +0900

    add pytest

--- test/test_transcribe.py
+++ test/test_transcribe.py
@@ -1,29 +1,24 @@
-import unittest
 import base64
 from unittest.mock import patch
+from src import transcribe
 
-class TestTranscribe(unittest.TestCase):
-    @patch('src_tmp.transcribe.whisper')
-    def test_base64_to_wavfile(self, mock_whisper):
+def test_base64_to_wavfile():
+    with patch('transcribe.whisper') as mock_whisper:
         # create a dummy load_model function
         mock_whisper.load_model.return_value = None
 
-        import transcribe
         with open('test/base64_audio', 'r') as f:
             base64_audio = f.read().strip()
 
         wav_file = transcribe.base64_to_wavfile(base64_audio)
 
         # Add your assertions here
-        self.assertIsNotNone(wav_file)
+        assert wav_file is not None
         print(f'wav_file is {wav_file}')  # print information about wav_file
-        self.assertEqual(wav_file.name, 'audio.wav')
+        assert wav_file.name == 'audio.wav'
         print('File name is correct')  # print a success message if the filename assertion passes
-        self.assertEqual(wav_file.read(), wav_file)
+        assert wav_file.read() == wav_file.getvalue()
         print('File is base64-encoded correctly')
 
-        self.assertEqual(wav_file.name, 'hoge.wav')
- 
+        assert wav_file.name != 'hoge.wav'
 
-if __name__ == '__main__':
-    unittest.main()
