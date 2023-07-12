25978043c47bd1adb62aa3b33fd2f149b3f544cb
Kazuki Kimura <kimura.kazuki44@gmail.com>
2023-07-13 07:47:13 +0900

    fix test_base64_to_wavfile()

--- test/test_transcribe.py
+++ test/test_transcribe.py
@@ -3,22 +3,13 @@ from unittest.mock import patch
 from src import transcribe
 
 def test_base64_to_wavfile():
-    with patch('src.transcribe.whisper') as mock_whisper:
-        # create a dummy load_model function
-        mock_whisper.load_model.return_value = None
+    with open('test/base64_audio', 'r') as f:
+        base64_audio = f.read().strip()
 
-        with open('test/base64_audio', 'r') as f:
-            base64_audio = f.read().strip()
+    with open('test/dialogue1.m4a', 'rb') as f:
+        original_audio_data = f.read()
 
-        wav_file = transcribe.base64_to_wavfile(base64_audio)
+    wav_file = transcribe.base64_to_wavfile(base64_audio)
 
-        # Add your assertions here
-        assert wav_file is not None
-        print(f'wav_file is {wav_file}')  # print information about wav_file
-        assert wav_file.name == 'audio.wav'
-        print('File name is correct')  # print a success message if the filename assertion passes
-        assert wav_file.read() == wav_file.getvalue()
-        print('File is base64-encoded correctly')
-
-        assert wav_file.name == 'hoge.wav'
-
+    assert wav_file.getvalue() == original_audio_data
+    print('Base64 decode matched with original audio data correctly')
