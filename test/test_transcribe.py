c6c668aa10b6fc4f6a994b410f2e731cbff3dc2b
Kazuki Kimura <kimura.kazuki44@gmail.com>
2023-07-12 17:29:07 +0900

    Update test_transcribe.py

--- test/test_transcribe.py
+++ test/test_transcribe.py
@@ -8,7 +8,7 @@ class TestTranscribe(unittest.TestCase):
         # create a dummy load_model function
         mock_whisper.load_model.return_value = None
 
-        from src.transcribe import base64_to_wavfile  # import after patching
+        from ..src.transcribe import base64_to_wavfile  # import after patching
 
         with open('test/base64_audio', 'r') as f:
             base64_audio = f.read().strip()
